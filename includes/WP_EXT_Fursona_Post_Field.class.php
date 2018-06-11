a8ca4c01c9b7b48f8e5f2bedd5b2a2194e0bf2b8
Kitsune Solar <kitsune.solar@gmail.com>
2018-06-12 02:48:41 +0300

    Update resource

---
+++ includes/WP_EXT_Fursona_Post_Field.class.php
@@ -0,0 +1,549 @@
+<?php
+
+/**
+ * Class WP_EXT_Fursona_Post_Field
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+class WP_EXT_Fursona_Post_Field extends WP_EXT_Fursona {
+
+	/**
+	 * Constructor.
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->run();
+	}
+
+	/**
+	 * Plugin: `initialize`.
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public function run() {
+		add_action( 'acf/init', [ $this, 'post_fields' ] );
+	}
+
+	/**
+	 * Post fields.
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public function post_fields() {
+		if ( function_exists( 'acf_add_local_field_group' ) ) {
+			acf_add_local_field_group( [
+				'key'                   => 'group_' . $this->pt_ID,
+				'title'                 => esc_html__( 'Информация о фурсоне', 'wp-ext-' . $this->domain_ID ),
+				'fields'                => [
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_general',
+						'label'             => esc_html__( 'Общие сведения', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_type',
+						'label'             => esc_html__( 'Тип персонажа', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_type',
+						'type'              => 'group',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'layout'            => 'table',
+						'sub_fields'        => [
+							[
+								'key'               => 'field_' . $this->pt_ID . '_species',
+								'label'             => esc_html__( 'Вид', 'wp-ext-' . $this->domain_ID ),
+								'name'              => 'species',
+								'type'              => 'text',
+								'instructions'      => 'Укажите вид персонажа.',
+								'required'          => 0,
+								'conditional_logic' => 0,
+								'wrapper'           => [
+									'width' => '',
+									'class' => '',
+									'id'    => '',
+								],
+								'default_value'     => '',
+								'placeholder'       => '',
+								'prepend'           => '',
+								'append'            => '',
+								'maxlength'         => '',
+							],
+							[
+								'key'               => 'field_' . $this->pt_ID . '_gender',
+								'label'             => esc_html__( 'Гендер', 'wp-ext-' . $this->domain_ID ),
+								'name'              => 'gender',
+								'type'              => 'text',
+								'instructions'      => 'Укажите гендер персонажа.',
+								'required'          => 0,
+								'conditional_logic' => 0,
+								'wrapper'           => [
+									'width' => '',
+									'class' => '',
+									'id'    => '',
+								],
+								'default_value'     => '',
+								'placeholder'       => '',
+								'prepend'           => '',
+								'append'            => '',
+								'maxlength'         => '',
+							],
+						],
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_body',
+						'label'             => esc_html__( 'Параметры персонажа', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_body',
+						'type'              => 'group',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'layout'            => 'table',
+						'sub_fields'        => [
+							[
+								'key'               => 'field_' . $this->pt_ID . '_height',
+								'label'             => esc_html__( 'Рост', 'wp-ext-' . $this->domain_ID ),
+								'name'              => 'height',
+								'type'              => 'number',
+								'instructions'      => 'Укажите рост персонажа.',
+								'required'          => 0,
+								'conditional_logic' => 0,
+								'wrapper'           => [
+									'width' => '',
+									'class' => '',
+									'id'    => '',
+								],
+								'default_value'     => '',
+								'placeholder'       => '',
+								'prepend'           => '',
+								'append'            => 'см.',
+								'min'               => '0',
+								'max'               => '',
+								'step'              => '',
+							],
+							[
+								'key'               => 'field_' . $this->pt_ID . '_weight',
+								'label'             => esc_html__( 'Вес', 'wp-ext-' . $this->domain_ID ),
+								'name'              => 'weight',
+								'type'              => 'number',
+								'instructions'      => 'Укажите вес персонажа.',
+								'required'          => 0,
+								'conditional_logic' => 0,
+								'wrapper'           => [
+									'width' => '',
+									'class' => '',
+									'id'    => '',
+								],
+								'default_value'     => '',
+								'placeholder'       => '',
+								'prepend'           => '',
+								'append'            => 'кг.',
+								'min'               => '0',
+								'max'               => '',
+								'step'              => '',
+							],
+							[
+								'key'               => 'field_' . $this->pt_ID . '_age',
+								'label'             => esc_html__( 'Возраст', 'wp-ext-' . $this->domain_ID ),
+								'name'              => 'age',
+								'type'              => 'number',
+								'instructions'      => 'Укажите возраст персонажа.',
+								'required'          => 0,
+								'conditional_logic' => 0,
+								'wrapper'           => [
+									'width' => '',
+									'class' => '',
+									'id'    => '',
+								],
+								'default_value'     => '',
+								'placeholder'       => '',
+								'prepend'           => '',
+								'append'            => 'лет',
+								'min'               => '0',
+								'max'               => '',
+								'step'              => '',
+							],
+						],
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_furry_code',
+						'label'             => esc_html__( 'Фуркод', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_furry_code',
+						'type'              => 'text',
+						'instructions'      => 'Введите фуркод персонажа (если имеется).',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+						'maxlength'         => '',
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_appearance',
+						'label'             => esc_html__( 'Внешность', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_appearance',
+						'label'             => esc_html__( 'Внешность', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_appearance',
+						'type'              => 'wysiwyg',
+						'instructions'      => 'Опишите внешние характеристики персонажа.',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'tabs'              => 'all',
+						'toolbar'           => 'basic',
+						'media_upload'      => 0,
+						'delay'             => 0,
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_character',
+						'label'             => esc_html__( 'Характер', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_character',
+						'label'             => esc_html__( 'Характер', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_character',
+						'type'              => 'wysiwyg',
+						'instructions'      => 'Опишите характер персонажа.',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'tabs'              => 'all',
+						'toolbar'           => 'basic',
+						'media_upload'      => 0,
+						'delay'             => 0,
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_hobbies',
+						'label'             => esc_html__( 'Увлечения', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_hobbies',
+						'label'             => esc_html__( 'Увлечения', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_hobbies',
+						'type'              => 'wysiwyg',
+						'instructions'      => 'Укажите, чем увлекается персонаж.',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'tabs'              => 'all',
+						'toolbar'           => 'basic',
+						'media_upload'      => 0,
+						'delay'             => 0,
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_abilities',
+						'label'             => esc_html__( 'Способности', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_abilities',
+						'label'             => esc_html__( 'Способности', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_abilities',
+						'type'              => 'wysiwyg',
+						'instructions'      => 'Укажите способности персонажа.',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'default_value'     => '',
+						'tabs'              => 'all',
+						'toolbar'           => 'basic',
+						'media_upload'      => 0,
+						'delay'             => 0,
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_cover',
+						'label'             => esc_html__( 'Обложка', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_cover',
+						'label'             => esc_html__( 'Обложка', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_cover',
+						'type'              => 'image',
+						'instructions'      => 'Загрузите обложку для персонажа.',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'return_format'     => 'array',
+						'preview_size'      => 'thumbnail',
+						'library'           => 'all',
+						'min_width'         => '',
+						'min_height'        => '',
+						'min_size'          => '',
+						'max_width'         => '',
+						'max_height'        => '',
+						'max_size'          => '',
+						'mime_types'        => 'jpg,png',
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_character_sheet',
+						'label'             => esc_html__( 'Лист персонажа', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_character_sheet',
+						'label'             => esc_html__( 'Лист персонажа', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_character_sheet',
+						'type'              => 'image',
+						'instructions'      => 'Загрузите лист персонажа (если имеется).',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'return_format'     => 'array',
+						'preview_size'      => 'thumbnail',
+						'library'           => 'all',
+						'min_width'         => '',
+						'min_height'        => '',
+						'min_size'          => '',
+						'max_width'         => '',
+						'max_height'        => '',
+						'max_size'          => '',
+						'mime_types'        => 'jpg,png',
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_gallery',
+						'label'             => esc_html__( 'Галерея', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_gallery',
+						'label'             => esc_html__( 'Галерея', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_gallery',
+						'type'              => 'gallery',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'min'               => '',
+						'max'               => '',
+						'insert'            => 'append',
+						'library'           => 'all',
+						'min_width'         => '',
+						'min_height'        => '',
+						'min_size'          => '',
+						'max_width'         => '',
+						'max_height'        => '',
+						'max_size'          => '',
+						'mime_types'        => 'jpg,png',
+					],
+					[
+						'key'               => 'tab_' . $this->pt_ID . '_meta',
+						'label'             => esc_html__( 'META-информация', 'wp-ext-' . $this->domain_ID ),
+						'name'              => '',
+						'type'              => 'tab',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'placement'         => 'left',
+						'endpoint'          => 0,
+					],
+					[
+						'key'               => 'field_' . $this->pt_ID . '_meta',
+						'label'             => esc_html__( 'META-теги', 'wp-ext-' . $this->domain_ID ),
+						'name'              => $this->pt_ID . '_meta',
+						'type'              => 'taxonomy',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => [
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						],
+						'taxonomy'          => $this->pt_ID . '_meta',
+						'field_type'        => 'multi_select',
+						'allow_null'        => 0,
+						'add_term'          => 0,
+						'save_terms'        => 1,
+						'load_terms'        => 0,
+						'return_format'     => 'id',
+						'multiple'          => 0,
+					],
+				],
+				'location'              => [
+					[
+						[
+							'param'    => 'post_type',
+							'operator' => '==',
+							'value'    => $this->pt_ID,
+						],
+					],
+				],
+				'menu_order'            => 0,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => 1,
+				'description'           => '',
+			] );
+		}
+	}
+}
+
+/**
+ * Helper function to retrieve the static object without using globals.
+ *
+ * @return WP_EXT_Fursona_Post_Field
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+function WP_EXT_Fursona_Post_Field() {
+	static $object;
+
+	if ( null == $object ) {
+		$object = new WP_EXT_Fursona_Post_Field;
+	}
+
+	return $object;
+}
+
+/**
+ * Initialize the object on `plugins_loaded`.
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+add_action( 'plugins_loaded', [ WP_EXT_Fursona_Post_Field(), 'run' ] );
---
+++ plugin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name:     (WP-EXT) Fursona
+ * Plugin URI:      https://metastore.pro/
+ *
+ * Description:     Fursona post type and more.
+ *
+ * Author:          Kitsune Solar
+ * Author URI:      https://kitsune.solar/
+ *
+ * Version:         1.0.0
+ *
+ * Text Domain:     wp-ext-fursona
+ * Domain Path:     /languages
+ *
+ * License:         GPLv3
+ * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Loading `WP_EXT_Fursona`.
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+function run_wp_ext_fursona() {
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_Post_Type.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_Post_Field.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_Taxonomy.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_User_Role.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_Theme.class.php' );
+//  require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_Template.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_ShortCode.class.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'includes/WP_EXT_Fursona_Widget.class.php' );
+}
+
+run_wp_ext_fursona();
